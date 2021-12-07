ff970d9d3b360791510b3799afb33acd2c43d020
DeVice233 <yakovenko182003@gmail.com>
2021-12-07 17:11:53 +1000

    Добавьте файлы проекта.

---
+++ Chernovik/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Chernovik.db;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chernovik
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //var connection = DBInstance.Get();
+            //string path = @"C:\Users\User\Desktop\Практика\Сессия 1\materialsupplier_b_import.csv";
+            //var materials = connection.Material.ToList();
+            //var types = connection.MaterialType.ToList();
+            //var supliers = connection.Supplier.ToList();
+            //var rows = File.ReadAllLines(path);
+
+            //connection.MaterialType.Add(new MaterialType { Title = "Пресс" });
+            //    connection.MaterialType.Add(new MaterialType { Title = "Нарезка" });
+            //    connection.MaterialType.Add(new MaterialType { Title = "Рулон" });
+            //    connection.MaterialType.Add(new MaterialType { Title = "Гранулы" });
+
+            //for (int i = 1; i < rows.Length; i++)
+            //{
+            //    var cols = rows[i].Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //    connection.Material.Add(new Material
+            //    {
+            //        Title = cols[0],
+            //        MaterialType = types.First(s => s.Title == cols[1]),
+            //        Image = cols[2],
+            //        Cost = decimal.Parse(cols[3]),
+            //        CountInStock = int.Parse(cols[4]),
+            //        MinCount = int.Parse(cols[5]),
+            //        CountInPack = int.Parse(cols[6]),
+            //        Unit = cols[7]
+            //    }) ;
+
+            //}
+            //for (int i = 1; i < rows.Length; i++)
+            //{
+            //    var cols = rows[i].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //    connection.Supplier.Add(new Supplier
+            //    {
+            //       Title = cols[0],
+            //       INN = cols[2],
+            //       StartDate = DateTime.Parse(cols[4]),
+            //       SupplierType = cols[1],
+            //       QualityRating = int.Parse(cols[3])
+            //    }) ;
+
+            //}
+        //    for (int i = 1; i < rows.Length; i++)
+        //    {
+        //        var cols = rows[i].Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+        //        Material material = materials.First(s => s.Title == cols[0]);
+
+        //        Supplier supplier = supliers.First(s => s.Title == cols[1]);
+                
+        //        supplier.Material.Add(material);
+                   
+             
+        //}
+
+        //    connection.SaveChanges();
+        }
+    }
+}
---
+++ Chernovik/db/DBInstance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.EntityClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chernovik.db
+{
+    public static class DBInstance
+    {
+        static ChernovikDbEntities connection;
+        static object objectLock = new object();
+        public static ChernovikDbEntities Get()
+        {
+            lock (objectLock)
+            {
+                if (connection == null)
+                    connection = new ChernovikDbEntities();
+                return connection;
+            }
+        }
+    }
+}
